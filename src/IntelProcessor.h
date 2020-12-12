0f645c3d456599b78a4b0d824c0b98ac7b2f42c5
Alexander Mangazeev <alex3287@bk.ru>
2020-12-12 20:08:44 +0700

    create class IntelProcessor

---
+++ src/IntelProcessor.h
@@ -0,0 +1,16 @@
+//
+// Created by Александр Мангазеев on 12.12.2020.
+//
+
+#ifndef COMPUTER_INTELPROCESSOR_H
+#define COMPUTER_INTELPROCESSOR_H
+
+
+
+class IntelProcessor {
+
+};
+
+
+
+#endif //COMPUTER_INTELPROCESSOR_H
