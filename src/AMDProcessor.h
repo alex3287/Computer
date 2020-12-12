f6e06b834c704b5a0624ae02cc6b3ca1560c8b7f
Alexander Mangazeev <alex3287@bk.ru>
2020-12-12 20:11:27 +0700

    create class AMDProcessor

---
+++ src/AMDProcessor.h
@@ -0,0 +1,14 @@
+//
+// Created by Александр Мангазеев on 12.12.2020.
+//
+
+#ifndef COMPUTER_AMDPOCESSOR_H
+#define COMPUTER_AMDPOCESSOR_H
+
+
+class AMDPocessor {
+
+};
+
+
+#endif //COMPUTER_AMDPOCESSOR_H
