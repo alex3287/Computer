20987e5cdafad928bd50461d1d7a87c1072bdbe8
Alexander Mangazeev <alex3287@bk.ru>
2020-12-13 01:13:58 +0700

    create methods

--- src/IProcessor.cpp
+++ src/IProcessor.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "IProcessor.h"
+
+IProcessor::~IProcessor() {}
+
+QString IProcessor::getVersion() {
+    return "Processor version ";
+}
+
+QString IProcessor::getType() {
+    return "Processor type ";
+}
+
+QString IProcessor::getSpeed() {
+    return "Processor speed ";
+}
+
+QString IProcessor::getProcessorInfo() {
+    return "Processor for " + getVersion() + getType() + getSpeed();
+}
--- src/IProcessor.h
+++ src/IProcessor.h
@@ -5,12 +5,16 @@
 #ifndef COMPUTER_IPROCESSOR_H
 #define COMPUTER_IPROCESSOR_H
 
-
+#include <QString>
 
 class IProcessor {
-
+public:
+    ~IProcessor();
+    virtual QString getVersion() = 0;
+    virtual QString getType() = 0;
+    virtual QString getSpeed() = 0;
+    virtual QString getProcessorInfo() = 0;
 };
-
 
 
 #endif //COMPUTER_IPROCESSOR_H
