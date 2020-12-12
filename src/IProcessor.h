fee4fc984e2b2655161d810de4bbcba42da80d52
Alexander Mangazeev <alex3287@bk.ru>
2020-12-12 20:06:33 +0700

    create class IProcessor

---
+++ src/IProcessor.h
@@ -0,0 +1,16 @@
+//
+// Created by Александр Мангазеев on 12.12.2020.
+//
+
+#ifndef COMPUTER_IPROCESSOR_H
+#define COMPUTER_IPROCESSOR_H
+
+
+
+class IProcessor {
+
+};
+
+
+
+#endif //COMPUTER_IPROCESSOR_H
