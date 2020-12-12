0556c7bcf9a71e2f44755f8c855cff8e42716f1c
Alexander Mangazeev <alex3287@bk.ru>
2020-12-12 20:05:33 +0700

    create class Computer

---
+++ src/Computer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Александр Мангазеев on 12.12.2020.
+//
+
+#include "Computer.h"
---
+++ src/Computer.h
@@ -0,0 +1,16 @@
+//
+// Created by Александр Мангазеев on 12.12.2020.
+//
+
+#ifndef COMPUTER_COMPUTER_H
+#define COMPUTER_COMPUTER_H
+
+
+
+class Computer {
+
+};
+
+
+
+#endif //COMPUTER_COMPUTER_H
