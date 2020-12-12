565c7def99ff976bf8c4992a95d4d21526f0612f
Alexander Mangazeev <alex3287@bk.ru>
2020-12-13 01:19:15 +0700

    create methonds in class

--- src/IntelProcessor.cpp
+++ src/IntelProcessor.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "IntelProcessor.h"
+
+IntelProcessor::IntelProcessor(QString version, int speed, int procType): version(version), speed(speed), procType(procType) {}
+
+QString IntelProcessor::getVersion() {
+    return "Processor version Intel";
+}
+
+QString IntelProcessor::getSpeed() {
+    return "Processor speed 10";
+}
+
+QString IntelProcessor::getType() {
+    return "Processor type testIntel ";
+}
--- src/IntelProcessor.h
+++ src/IntelProcessor.h
@@ -5,12 +5,20 @@
 #ifndef COMPUTER_INTELPROCESSOR_H
 #define COMPUTER_INTELPROCESSOR_H
 
-
+#include "IProcessor.h"
 
 class IntelProcessor {
+private:
+    QString version;
+    int speed;
+    int procType; //fixme
 
+public:
+    IntelProcessor(QString version, int speed, int procType);
+    QString getVersion();
+    QString getType();
+    QString getSpeed();
 };
-
 
 
 #endif //COMPUTER_INTELPROCESSOR_H
