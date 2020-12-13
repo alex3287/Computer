bb6dcf586d9cf49142987d5dd4907a260e51a373
Alexander Mangazeev <alex3287@bk.ru>
2020-12-14 00:24:04 +0700

    add test for program

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <QString>
-#include <iostream>
+#include <QTextStream>
+#include "IntelProcessor.h"
+
+enum ProcessorType
+{
+    x86,
+    x64
+};
 
 int main() {
-    QString russianString("Русский текст");
-    std::cout << russianString.toStdString() << std::endl;
+    QTextStream out(stdout);
+    out<<"hello world"<<endl;
+    ProcessorType s = x64;
+    out<<s<<endl;
+    IntelProcessor Ab("Test", 15, IProcessor::x64);
+    out<<Ab.getVersion()<<endl;
+    out<<Ab.getProcessorInfo()<<endl;
+
     return 0;
 }
