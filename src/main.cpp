987002145169f979688407f6550bc9e4a8ef7cda
Alexander Mangazeev <alex3287@bk.ru>
2020-12-17 22:43:11 +0700

    new tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <QString>
 #include <QTextStream>
 #include "IntelProcessor.h"
+#include "AMDProcessor.h"
 
 enum ProcessorType
 {
@@ -13,9 +14,12 @@ int main() {
     out<<"hello world"<<endl;
     ProcessorType s = x64;
     out<<s<<endl;
-    IntelProcessor Ab("Test", 15, IProcessor::x64);
+    IntelProcessor Ab("Intel-1", 15, IProcessor::x64);
     out<<Ab.getVersion()<<endl;
     out<<Ab.getProcessorInfo()<<endl;
+
+    AMDProcessor amdProc("AMD-1", 20, IProcessor::x86);
+    out<<amdProc.getVersion()<<endl;
 
     return 0;
 }
